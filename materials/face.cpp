1a38204c9fff05a1fa087fec9bd4dddaa994219f
Jonah Lyu <lvjikeleo@hotmail.com>
2019-12-01 12:18:59 +0000

    face detection jpg

--- materials/face.cpp
+++ materials/face.cpp
@@ -33,6 +33,8 @@ int main( int argc, const char** argv )
 {
        // 1. Read Input Image
 	Mat frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+	String s = argv[1];
+	s = "face_" + s;
 
 	vector<Rect> gTs;
 
@@ -51,7 +53,7 @@ int main( int argc, const char** argv )
 	// Draw ground truth rectangles on image
 	groundTruthDraw(frame, gTs);
 	// 4. Save Result Image
-	imwrite( "detected.jpg", frame );
+	imwrite( s, frame );
 
 	return 0;
 }
@@ -65,7 +67,7 @@ int main( int argc, const char** argv )
 // }
 
 bool rectIntersect(Rect r1, Rect r2, double thresh) {
-	//Returns a boolean indicating if the area 
+	//Returns a boolean indicating if the area
 	//shared by both rectangles is greater than the threshold
 	return ((float) (r1 & r2).area()/(r1|r2).area() > thresh);
 }
@@ -90,6 +92,9 @@ vector<Rect> getTruths(int index) {
 	gTs[2].push_back(Rect(412, 123, 122, 122));
 	gTs[3].push_back(Rect(461, 216, 103, 103));
 	gTs[3].push_back(Rect(726, 189, 100, 100));
+	gTs[4].push_back(Rect(360, 110, 100, 100));
+	gTs[4].push_back(Rect(548, 121, 54, 54));
+	gTs[4].push_back(Rect(65, 123, 86, 86));
 	for (int j = 0; j < gTs[index].size(); j++) { //Expanding ground truth rect so detected can be seen
 		gTs[index][j].x -= 4;
 		gTs[index][j].y -= 4;
@@ -118,7 +123,7 @@ void detectAndDisplay( Mat frame , vector<Rect> gt)
 	cvtColor( frame, frame_gray, CV_BGR2GRAY );
 	equalizeHist( frame_gray, frame_gray );
 
-	// 2. Perform Viola-Jones Object Detection 
+	// 2. Perform Viola-Jones Object Detection
 	cascade.detectMultiScale( frame_gray, faces, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
 
        // 3. Print number of Faces found
